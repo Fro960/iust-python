9006bf6de7cdcacac45327398aa4bf9676b74655
Fro960 <qazifurqan106@gmail.com>
2026-01-19 00:34:30 +0530

    added input check

--- private/gemini.py
+++ private/gemini.py
@@ -4,10 +4,15 @@ from google.genai import types
 
 client = genai.Client(api_key="AIzaSyC-2Fd1G667b7WTWbyGP1CFKf9xgkQtDlM")
 
+if len(sys.argv) == 2:
+    res = sys.argv[1]
+else:
+    res = input("ask: ")
+
 response = client.models.generate_content(
     model="gemini-3-flash-preview",
     config=types.GenerateContentConfig(
     system_instruction="answer in one sentence"),
-    contents= sys.argv[1]
+    contents= res
 )
 print(response.text)
