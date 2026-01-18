59a9b41e1b49f5d674192eb523c083b02c1e3730
Fro960 <qazifurqan106@gmail.com>
2026-01-19 00:24:39 +0530

    added system instructios to gemini

--- private/gemini.py
+++ private/gemini.py
@@ -1,8 +1,13 @@
+import sys
 from google import genai
+from google.genai import types
 
 client = genai.Client(api_key="AIzaSyC-2Fd1G667b7WTWbyGP1CFKf9xgkQtDlM")
 
 response = client.models.generate_content(
-    model="gemini-3-flash-preview", contents="Explain how AI works in a few words"
+    model="gemini-3-flash-preview",
+    config=types.GenerateContentConfig(
+    system_instruction="answer in one sentence"),
+    contents= sys.argv[1]
 )
 print(response.text)
