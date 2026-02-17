9f008a72e3698e39e9fb7d09118e6cc3193c7fe5
Fro960 <106296793+Fro960@users.noreply.github.com>
2026-02-17 14:42:33 +0530

    test file

--- private/test.py
+++ private/test.py
@@ -1,23 +1,11 @@
-if __name__ == '__main__':
-    lst = []
-    N = int(input())
-    for _ in range(N):
-        command = input().split()
-        cmd = command[0]
-        match cmd:
-            case "insert":
-                lst.insert(int(command[1]), int(command[2]))
-            case "remove":
-                lst.remove(int(command[1]))
-            case "append":
-                lst.append(int(command[1]))
-            case "sort":
-                lst.sort()
-            case "pop":
-                lst.pop()
-            case "reverse":
-                lst.sort(reverse = True) 
-            case "print":
-                print(lst)
-    
-    
+import math
+
+def get_factors(n):
+    factors = set()
+    for i in range(2, math.isqrt(n) + 1):
+        if n % i == 0:
+            factors.add(i)
+            factors.add(n // i)
+    return factors
+
+print(get_factors(20))
