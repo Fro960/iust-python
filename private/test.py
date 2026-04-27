a50fe1ce95e8ffc1593890944cf60785a4f90295
Fro960 <106296793+Fro960@users.noreply.github.com>
2026-04-28 00:26:35 +0530

    Added some files

--- private/test.py
+++ private/test.py
@@ -1,11 +1,2 @@
-import math
-
-def get_factors(n):
-    factors = set()
-    for i in range(2, math.isqrt(n) + 1):
-        if n % i == 0:
-            factors.add(i)
-            factors.add(n // i)
-    return factors
-
-print(get_factors(20))
+l = list(("farhan", "jazim", "inayat"))
+print(type(l))
